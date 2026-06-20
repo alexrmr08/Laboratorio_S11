c0274683b06540786f5e777c08dd715e64948db4
alexrmr08 <walter.romero123.2@gmail.com>
2026-06-20 16:38:47 -0500

    feat: Implementar el cafe de batido con sus precios y tamaños

---
+++ Batido.java
@@ -0,0 +1,15 @@
+public class Batido extends Cafe {{
+
+    private static final double[] PRECIOS = {0.89, 0.99, 1.09}; // N, M, G
+
+    public Batido(Tamano tamano) {
+        super(tamano);
+        descripcion = "Cafe Batido";
+    }
+
+    @Override
+    public double costo() {
+        return PRECIOS[tamano.ordinal()];
+    }
+
+}
