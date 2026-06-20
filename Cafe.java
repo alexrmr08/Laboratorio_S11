619c20d379f39d14aa67b6764bfe657a5cde3467
alexrmr08 <walter.romero123.2@gmail.com>
2026-06-20 16:03:31 -0500

    feat: Implementar la clase abstracta Cafe

--- Cafe.java
+++ Cafe.java
@@ -1,5 +1,22 @@
 public abstract class Cafe {
     
-    
+    public enum Tamano { NORMAL, MEDIANO, GRANDE }
+
+    protected String descripcion = "Bebida desconocida";
+    protected Tamano tamano;
+
+    public Cafe(Tamano tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Tamano getTamano() {
+        return tamano;
+    }
+
+    public abstract double costo();
 
 }
